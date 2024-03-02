a584f07ae1061a90317a3666e108792830ae84f5
h7ml <h7ml@qq.com>
2024-03-02 15:11:38 +0800

    chore: delete build src *.js

--- src/core/command.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.registerCommands = void 0;
-var commander_1 = require("commander");
-var action_1 = require("./action");
-var registerCommands = function () {
-    commander_1.program.argument('<message>', '需要翻译的信息').action(action_1.translationAcion);
-};
-exports.registerCommands = registerCommands;
